72228373a4d3eccfa48e0724c216e79601383046
jerjerzanne <simon.roy5219@gmail.com>
2018-05-25 07:19:02 -0700

    created the docs folder

--- gulpfile.js
+++ gulpfile.js
@@ -31,4 +31,4 @@ gulp.task('serve', ['sass'], function() {
     gulp.watch("src/test.html").on('change', browserSync.reload);
 });
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass','js']);
