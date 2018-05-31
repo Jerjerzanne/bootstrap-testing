611d4b928ddf868c8b45466f6952971e99e5e9c7
jerjerzanne <simon.roy5219@gmail.com>
2018-05-31 07:29:08 -0700

    fixing the git search issue

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,10 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
-//Utility
-
-var glob = require('glob');
-
-//Testing
-
-//var pa11y = require('pa11y-ci');
-
 
 //Building the css for the distribution folder
 gulp.task('sass', function () {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', '_scss/combined.scss'])
+    return gulp.src(['_scss/combined.scss'])
         .pipe(sass())
         .pipe(gulp.dest("dist/css"));
 
@@ -22,15 +14,14 @@ gulp.task('sass', function () {
 //Building the js for the distribution folder
 gulp.task('js', function () {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest("dist/js"))
-        .pipe(browserSync.stream());
+        .pipe(gulp.dest("dist/js"));
 });
 
 //==================================
 //          Currently Unused
 //==================================
 
-
+/* 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function () {
 
@@ -44,29 +35,7 @@ gulp.task('serve', ['sass'], function () {
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/_scss/*.scss'], ['sass']);
     gulp.watch("src/test.html").on('change', browserSync.reload);
 });
-
-//tests all html files of the static site
-gulp.task('files',{
-    allowedStandards: 'WCAG2AA',
-    level: 'error',
-    chromeLaunchConfig: {
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox'
-        ]
-    }
-}, function () {
-    glob('./_site/**/*.html', function (err, files) {
-        console.log(files);
-        for (i = 0; i < files.length; i++) {
-            pa11y(files[i]).then((results) => {
-                const cliResults = cli.results(results);
-                console.log(cliResults);
-            });
-        };
-
-    });
-});
+ */
 
 
 gulp.task('default', ['sass', 'js']);
